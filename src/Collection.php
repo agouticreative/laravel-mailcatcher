3a4802d0d41a751c652a70c16238a6b294cb83c6
Travis Morrison <travismmorrison@gmail.com>
2016-06-14 20:09:14 -0400

    adding a search for recipients to Collections

--- src/Collection.php
+++ src/Collection.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Collection as LaravelCollection;
 
 class Collection extends LaravelCollection
 {
-    public function __construct($messages)
+    public function __construct(array $messages = [])
     {
         $messageModels = [];
 
@@ -22,5 +22,17 @@ class Collection extends LaravelCollection
         foreach ($this as $message) {
             $message->delete();
         }
+    }
+
+    public function whereRecip($email)
+    {
+        $coll = new self;
+        foreach ($this as $msg) {
+            if ($msg->recipients->contains('email', $email)) {
+                $coll->push($msg);
+            }
+        }
+        return $coll;
+
     }
 }
