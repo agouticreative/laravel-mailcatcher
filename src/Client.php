1b3a799d493b09704a6ed0027408d6c3b24d085e
Travis Morrison <travismmorrison@gmail.com>
2016-06-14 20:08:32 -0400

    adding get() to Client so as to be more eloquent-like. It is an alias for search() and nothing more

--- src/Client.php
+++ src/Client.php
@@ -7,11 +7,17 @@ use Alex\MailCatcher\Client as MailCatcherClient;
 
 class Client extends MailCatcherClient
 {
+
     public function __construct()
     {
         $url = Config::get('mailcatcher.url');
 
         return parent::__construct($url);
+    }
+
+    public function get(array $criterias = [], $limit = null)
+    {
+        return $this->search($criterias, $limit);
     }
 
     public function search(array $criterias = [], $limit = null)
